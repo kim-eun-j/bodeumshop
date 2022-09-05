2e33264d6c1f818491c3f440f01dc2c69a58d264
kim-eun-j <111939826+kim-eun-j@users.noreply.github.com>
2022-09-05 09:22:07 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -97,44 +97,4 @@ $(function () {
             }
         });
     })
-
-    //mobile menu
-    $("#nav>li>a").click(function () {
-        $(this).next().slideToggle(300)
-            .parent()
-            .siblings()
-            .children(".sub").slideUp(300);
-        $(this).parents().toggleClass("active")
-            .siblings().removeClass("active")
-    })
-
-    //nav 나오고 들어가고
-    $(".ham").on("click", function () {
-        $("nav, .leftGnb, .close img").stop().animate({
-            left: 0
-        }, 500)
-    })
-    $(".close").on("click", function () {
-        $("nav, .leftGnb, .close img").stop().animate({
-            left: -100 + "%"
-        }, 500)
-    })
-
-    var button2 = false;
-    $(".icon").on("click", function () {
-        if (button2 == false) {
-            $(".rightGnb").stop().animate({
-                right: 0
-            }, 500)
-            button2 = true;
-        }
-        else {
-            $(".rightGnb").stop().animate({
-                right: -100 + "%"
-            }, 500)
-            button2 = false;
-        }
-
-    })
-
 })
